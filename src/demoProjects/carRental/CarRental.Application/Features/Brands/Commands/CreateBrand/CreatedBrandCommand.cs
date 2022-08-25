5592fbb2922781c7ef32d815a6f19b04a6f39672
Ramis Yüksel <ramisyk97@gmail.com>
2022-08-25 23:12:51 +0300

    created brand commands

---
+++ src/demoProjects/carRental/CarRental.Application/Features/Brands/Commands/CreateBrand/CreatedBrandCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using AutoMapper;
+using CarRental.Application.Features.Brands.Dtos;
+using CarRental.Application.Features.Brands.Rules;
+using CarRental.Application.Services.Repositories;
+using CarRental.Domain.Entities;
+using MediatR;
+
+namespace CarRental.Application.Features.Brands.Commands.CreateBrand
+{
+	public partial class CreatedBrandCommand : IRequest<CreatedBrandDto>
+	{
+
+        public string Name { get; set; }
+
+        public class CreateBrandCommandHandler : IRequestHandler<CreatedBrandCommand, CreatedBrandDto>
+        {
+            private readonly IBrandRepository _brandRepository;
+            private readonly IMapper _mapper;
+            private readonly BrandBusinessRules _brandBusinessRules;
+
+            public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper, BrandBusinessRules brandBusinessRules)
+            {
+                _brandRepository = brandRepository;
+                _mapper = mapper;
+                _brandBusinessRules = brandBusinessRules;
+            }
+
+            public async Task<CreatedBrandDto> Handle(CreatedBrandCommand request, CancellationToken cancellationToken)
+            {
+                await _brandBusinessRules.BrandNameCanNotBeDuplicatedWhenInserted(request.Name);
+
+                Brand mappedBrand = _mapper.Map<Brand>(request);
+                Brand createdBrand = await _brandRepository.AddAsync(mappedBrand);
+                CreatedBrandDto createdBrandDto = _mapper.Map<CreatedBrandDto>(createdBrand);
+
+                return createdBrandDto;
+
+            }
+        }
+    }
+}
+
---
+++ src/demoProjects/carRental/CarRental.Application/Features/Brands/Rules/BrandBusinessRules.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CarRental.Application.Services.Repositories;
+using CarRental.Domain.Entities;
+using Core.Persistence.Paging;
+using Core.CrossCuttingConcerns.Exceptions;
+
+namespace CarRental.Application.Features.Brands.Rules
+{
+	public class BrandBusinessRules
+	{
+        private readonly IBrandRepository _brandRepository;
+
+        public BrandBusinessRules(IBrandRepository brandRepository)
+        {
+            _brandRepository = brandRepository;
+        }
+
+        public async Task BrandNameCanNotBeDuplicatedWhenInserted(string name)
+        {
+            IPaginate<Brand> result = await _brandRepository.GetListAsync(b => b.Name == name);
+            if (result.Items.Any()) throw new BusinessException("Brand name exists.");
+        }
+
+    }
+}
+
